dda7bb25c4f1d9465034e848d3e433f9294da26e
Omar Shaikh <oshaikh13@gmail.com>
2016-07-21 20:40:18 -0400

    Add function for pitch/yaw/roll from a quaternion

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
 
+// Radians.
+function getAngles(x, y, z) {
+  var obj = {}
+  obj.roll  = Mathf.Atan2(2*y*w - 2*x*z, 1 - 2*y*y - 2*z*z);
+  obj.pitch = Mathf.Atan2(2*x*w - 2*y*z, 1 - 2*x*x - 2*z*z);
+  obj.yaw   =  Mathf.Asin(2*x*y + 2*z*w);
+  return obj;
+}
+
 app.get('/', function (req, res) {
   res.send('Active!');
 });
 
 app.post('/position', function (req, res) {
-  res.send('Active!');
+
 });
 
 
