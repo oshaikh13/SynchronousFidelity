57230077380d17f3a25d27a5b0b3f5105adbb428
Omar Shaikh <oshaikh13@gmail.com>
2017-01-14 17:18:01 +0300

    comments in actionCache.js

--- server/actions/actionCache.js
+++ server/actions/actionCache.js
@@ -1,26 +1,26 @@
+// a cache for actions from the client
+
 module.exports = {
 
   cache: {},
 
-  limit: 2000, // Store 2000 frames in memory
+  limit: 2000, // change this to change the number of frames stored in a cache for each independent user
 
   add: function (data) {
 
     var name = data.displayName;
-
+    
     if (!this.cache[name]) this.cache[name] = [];
-
     this.cache[name].push(data);
-
+    
     var numRemove = this.cache[name].length - this.limit;
-
-    console.log(numRemove);
 
     if (numRemove > 0) this.cache[name] = this.cache[name].splice(numRemove);
     
   },
 
-  // TODO: Binary Search for T1 and T2 and slice...
+  // TODO: Binary Search for T1 and T2 and slice may be more efficient...
+  // filter uses a linear search
   get: function (username, comparator, t1, t2) {
 
     var inTimestamps = function (element) {
@@ -28,7 +28,6 @@ module.exports = {
     }
 
     if (!this.cache[username]) this.cache[username] = [];
-    
     if (!this.cache[comparator]) this.cache[comparator] = [];
 
     var inLimitUser = this.cache[username].filter(inTimestamps);
