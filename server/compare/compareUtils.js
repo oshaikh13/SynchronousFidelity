a15cedf45fa7ede11065b3e1b5d05092f85767a7
Omar Shaikh <oshaikh13@gmail.com>
2017-01-20 19:23:59 +0300

    Fix weird spacing

--- server/compare/compareUtils.js
+++ server/compare/compareUtils.js
@@ -181,24 +181,24 @@ module.exports = {
     
     var shortestArrayLength = 0;
      
-    if(x.length == y.length) {
-        shortestArrayLength = x.length;
-    } else if(x.length > y.length) {
-        shortestArrayLength = y.length;
-        // console.error('x has more items in it, the last ' + (x.length - shortestArrayLength) + ' item(s) will be ignored');
+    if (x.length == y.length) {
+      shortestArrayLength = x.length;
+    } else if (x.length > y.length) {
+      shortestArrayLength = y.length;
+      // console.error('x has more items in it, the last ' + (x.length - shortestArrayLength) + ' item(s) will be ignored');
     } else {
-        shortestArrayLength = x.length;
-        // console.error('y has more items in it, the last ' + (y.length - shortestArrayLength) + ' item(s) will be ignored');
+      shortestArrayLength = x.length;
+      // console.error('y has more items in it, the last ' + (y.length - shortestArrayLength) + ' item(s) will be ignored');
     }
   
     var xy = [];
     var x2 = [];
     var y2 = [];
   
-    for(var i=0; i<shortestArrayLength; i++) {
-        xy.push(x[i] * y[i]);
-        x2.push(x[i] * x[i]);
-        y2.push(y[i] * y[i]);
+    for (var i = 0; i < shortestArrayLength; i++) {
+      xy.push(x[i] * y[i]);
+      x2.push(x[i] * x[i]);
+      y2.push(y[i] * y[i]);
     }
   
     var sum_x = 0;
@@ -207,12 +207,12 @@ module.exports = {
     var sum_x2 = 0;
     var sum_y2 = 0;
   
-    for(var i=0; i< shortestArrayLength; i++) {
-        sum_x += x[i];
-        sum_y += y[i];
-        sum_xy += xy[i];
-        sum_x2 += x2[i];
-        sum_y2 += y2[i];
+    for (var i = 0 ; i < shortestArrayLength; i++) {
+      sum_x += x[i];
+      sum_y += y[i];
+      sum_xy += xy[i];
+      sum_x2 += x2[i];
+      sum_y2 += y2[i];
     }
   
     var step1 = (shortestArrayLength * sum_xy) - (sum_x * sum_y);
