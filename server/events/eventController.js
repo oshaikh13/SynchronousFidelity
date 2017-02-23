8f33afdd53629eb77018ed0418f3bae7673f0a97
Omar Shaikh <oshaikh13@gmail.com>
2017-02-23 19:38:50 +0300

    Update eventController to handle specific event queries

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -16,9 +16,20 @@ module.exports = {
   },
 
   getEvents: function(req, res, next) {
-    Event.find({}, function(err, doc) {
+
+    var query = {};
+
+    // The '+' operator converts a string to an integer
+    if (+req.query.t1 && +req.query.t2) 
+      query["timestamp"] = { $gte: +req.query.t1, $lte: +req.query.t2};
+
+    if (req.query.searchTerms) 
+      query["eventName"] = { "$regex": req.query.searchTerms, "$options": "i" };
+
+    Event.find(query, function(err, doc) {
       if (err) res.status(400).send(err);
       else res.send(doc);
     })
+
   }
 }
