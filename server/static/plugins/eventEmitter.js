47f79e90eab7fbd6c7312952010542299632806f
Omar Shaikh <oshaikh13@gmail.com>
2017-03-01 15:27:35 +0300

    move plugin folder

---
+++ server/static/plugins/eventEmitter.js
@@ -0,0 +1,57 @@
+
+// var baseURL = "http://localhost:8000/"
+
+// The name of the new mapping
+var MAPPING_NAME = "com.synchronousfidelity.events.controller";
+
+var eventNumber = 1;
+
+var DEBUG = false;
+
+// Create a new mapping object
+var mapping = Controller.newMapping(MAPPING_NAME);
+
+// Add a route to the mapping object
+// Events are fired on Right Trigger presses
+
+mapping.from(Controller.Standard.RightGrip).to(function (value) {
+
+
+  if (value !== 1) {
+    // Button is "unpressed"
+    return;
+  }
+
+  var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+  // you need to make a new instance for every HTTP request
+  
+  if (DEBUG) return;
+
+  xmlhttp.onreadystatechange = function () {
+    print("Saved to server with response code: " + xmlhttp.status);
+  };
+
+  var req = {
+    eventName: "Event " + eventNumber,
+    displayName: MyAvatar.displayName
+  };
+
+  Window.alert("Fired Event #" + eventNumber)
+
+  eventNumber++;
+
+  xmlhttp.open("POST", baseURL + "api/event/create");
+  xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xmlhttp.send(JSON.stringify(req));
+
+  print("FIRED EVENT " + eventNumber);
+
+});
+
+//Enable the new mapping
+Controller.enableMapping(MAPPING_NAME);
+
+// Disable the new mapping when the script ends
+Script.scriptEnding.connect(function () {
+  Controller.disableMapping(MAPPING_NAME);
+});
--- server/static/plugins/pluginTextContent.js
+++ server/static/plugins/pluginTextContent.js
@@ -1,15 +1,21 @@
 
+var fs = require('fs')
+
+var actionEmitterText = fs.readFileSync(__dirname + '/actionEmitter.js', 'utf8');
+var eventEmitterText = fs.readFileSync(__dirname + '/eventEmitter.js', 'utf8');
+
 module.exports = {
   getActionEmitterText: function (serverIpAddress) {
-    // Long, minified, js file :)
-    var scriptContent = `function avoidSpam(a){frame%SPAM_FRAME_COUNT===0&&a()}function replaceQuaternions(a,b){var c=Quat.safeEulerAngles(a);return b.pitch=c.x,b.yaw=c.y,b.roll=c.z,b}function sendToServer(a){if(a){var b=new XMLHttpRequest;b.onreadystatechange=function(){var a=!1;a=b.status,avoidSpam(function(){print("Saved to server with response code: "+a)})},b.open("POST",baseURL+"api/action/create"),b.setRequestHeader("Content-Type","application/json;charset=UTF-8"),b.send(JSON.stringify(a))}}function getPositions(){var a=function(){frame++};if(frame%FRAME_SEND_COUNT===0){var b={displayName:MyAvatar.displayName,hands:{leftHand:{position:MyAvatar.getLeftHandPosition(),pose:MyAvatar.getLeftHandPose()},rightHand:{position:MyAvatar.getRightHandPosition(),pose:MyAvatar.getRightHandPose()}},palms:{rightPalm:{position:MyAvatar.getRightPalmPosition(),rotation:MyAvatar.getRightPalmRotation()},leftPalm:{position:MyAvatar.getLeftPalmPosition(),rotation:MyAvatar.getLeftPalmRotation()}},head:{position:MyAvatar.getHeadPosition(),pitch:MyAvatar.headRoll,yaw:MyAvatar.headYaw,roll:MyAvatar.headPitch},body:{pitch:MyAvatar.bodyPitch,yaw:MyAvatar.bodyYaw,roll:MyAvatar.bodyRoll}};b.hands;for(var d in b.hands){var e=b.hands[d],f=e.pose.rotation;e=replaceQuaternions(f,e),e.pose=void 0}for(var g in b.palms){var h=b.palms[g],i=h.rotation;h=replaceQuaternions(i,h),h.rotation=void 0}avoidSpam(function(){JSON.stringify(b)}),sendToServer(b)}a()}var baseURL="` + serverIpAddress + `",frame=0,DEBUG=!1,SPAM_FRAME_COUNT=50,FRAME_SEND_COUNT=3;Script.update.connect(getPositions);`
+
+    var scriptContent =  `\nvar baseURL = "` + serverIpAddress + `";\n` + actionEmitterText;
     return scriptContent;
 
   },
 
   getEventEmitterText: function (serverIpAddress) {
 
-    var scriptContent = `var baseURL="` + serverIpAddress + `",MAPPING_NAME="com.synchronousfidelity.events.controller",eventNumber=1,DEBUG=!1,mapping=Controller.newMapping(MAPPING_NAME);mapping.from(Controller.Standard.RightGrip).to(function(e){if(1===e){var n=new XMLHttpRequest;if(!DEBUG){n.onreadystatechange=function(){print("Saved to server with response code: "+n.status)};var t={eventName:"Event "+eventNumber,displayName:MyAvatar.displayName};Window.alert("Fired Event #"+eventNumber),eventNumber++,n.open("POST",baseURL+"api/event/create"),n.setRequestHeader("Content-Type","application/json;charset=UTF-8"),n.send(JSON.stringify(t)),print("FIRED EVENT "+eventNumber)}}}),Controller.enableMapping(MAPPING_NAME),Script.scriptEnding.connect(function(){Controller.disableMapping(MAPPING_NAME)});`
+    var scriptContent = `\nvar baseURL = "` + serverIpAddress + `";\n` + actionEmitterText;
     return scriptContent;
+
   }
 }
