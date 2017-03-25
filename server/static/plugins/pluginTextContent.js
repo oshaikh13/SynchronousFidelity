d977c3755d18febd76f24818fad48c0b48792eed
Omar Shaikh <oshaikh13@gmail.com>
2017-03-25 17:52:51 +0300

    Fix server ip variable

--- server/static/plugins/pluginTextContent.js
+++ server/static/plugins/pluginTextContent.js
@@ -6,8 +6,10 @@ var eventEmitterText = fs.readFileSync(__dirname + '/eventEmitter.js', 'utf8');
 var synchronyColorCubeText = fs.readFileSync(__dirname + '/synchronyColorCube.js', 'utf8');
 
 
-function baseURLAttacher (script, serverIpAddress) {
-  return `\nvar baseURL = "` + serverIpAddress + `";\n` + script;
+function baseURLAttacher (serverIpAddress, script) {
+  // Somewhat dangerous. Just assume the serverIP isn't javascript code, because you're running this locally.
+  // If you're not, hardcode the serverIp please.
+  return "\nvar baseURL = \""+ serverIpAddress + "\";\n" + script;
 }
 
 module.exports = {
