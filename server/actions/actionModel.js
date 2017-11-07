e9454480986780a0c0638c1712ee256a6f9a7b6f
Omar Shaikh <oshaikh13@gmail.com>
2017-11-07 09:21:09 +0300

    adjust mongo model

--- server/actions/actionModel.js
+++ server/actions/actionModel.js
@@ -7,48 +7,19 @@ var ActionSchema = new mongoose.Schema({
   hands: {
     leftHand : {
       position: {},
-      pitch: Number,
-      yaw: Number,
-      roll: Number
+      rotation: {}
     },
 
     rightHand : {
       position: {},
-      pitch: Number,
-      yaw: Number,
-      roll: Number
+      rotation: {}
     }
   },
 
-  body: {
-    pitch: Number,
-    yaw: Number,
-    roll: Number
-  },
-
-  palms: {   
-   
-    rightPalm: {    
-      position: {},   
-      pitch: Number,    
-      yaw: Number,    
-      roll: Number    
-    },    
-
-    leftPalm: {   
-      position: {},   
-      pitch: Number,    
-      yaw: Number,    
-      roll: Number    
-    }   
-   
- },
 
   head: {
     position: {},
-    pitch: Number,
-    yaw: Number,
-    roll: Number
+    rotation: {}
   },
 
 
